124e38ef309bea2d462194431fbb67c8303f937c
JamesFisken <99928794+JamesFisken@users.noreply.github.com>
2022-06-08 18:52:16 +1200

    doesn't evolve/train yet

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import time
 import numpy as np
 import math
+import MnistDataSet
 
 start_time = time.time()
 
@@ -29,17 +30,14 @@ truthtable = [0, 0, 1]
 NN_layout = [4, 3, 3]
 NN_length = len(NN_layout)
 
-given_inputs = [1, 2, 3, 2.5]
-
-inputlayer = []
-hiddenlayers = []
-outputlayer = []
 NN=[]
 
 weights = []
 values = []
 bias = []
 def init(inputs):
+    inputlayer = []
+    outputlayer = []
     for i in range(NN_layout[0]):
         inputlayer.append(i_node(inputs[i], NN_layout[1]))
     NN.append(inputlayer)
@@ -71,16 +69,15 @@ def display_NN():
         print("hiddenlayer:", i)
         for node in NN[i+1]:
             print(i, node.value, node.weights, node.bias)
-
     print("-----------------------outputlayer-----------------------")
-    for node in outputlayer:
+    for node in NN[-1]:
         print(node.number, node.value)
     print("")
 
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
-def forward_propogate(values, weights, bias):
+def dotproduct(values, weights, bias):
     return np.dot(weights, values) + bias
 
 def cost(expected, actual):
@@ -91,23 +88,27 @@ def cost(expected, actual):
         elif a == 0:
             total += ((actual[i] - 0) ** 2)
     return total
+def foward_propagate(NN):
+    for n in range(NN_length-1):
+        values = np.array([NN[n][i].value for i in range(len(NN[n]))])
+        weights = np.array([NN[n][i].weights for i in range(len(NN[n]))]).transpose()
+        bias = np.array([NN[n+1][i].bias for i in range(len(NN[n+1]))])
+
+        result = dotproduct(values, weights, bias)
+        for i, v in enumerate(result):
+            NN[n+1][i].value = sigmoid(v)
+    output = [NN[-1][x].value for x in range(len(NN[-1]))]
+    fcost = cost(truthtable, output)
+    print("cost:", fcost)
+    return fcost
+
+given_inputs = [1, 2, 3, 2.5]
 
 init(given_inputs)
 adjust_modifiers(NN, 1)
+fcost = foward_propagate(NN)
+adjust_modifiers(NN, fcost)
+
+foward_propagate(NN)
 display_NN()
-
-for n in range(NN_length-1):
-    values = np.array([NN[n][i].value for i in range(len(NN[n]))])
-    weights = np.array([NN[n][i].weights for i in range(len(NN[n]))]).transpose()
-    bias = np.array([NN[n+1][i].bias for i in range(len(NN[n+1]))])
-
-    result = forward_propogate(values, weights, bias)
-    for i, v in enumerate(result):
-        NN[n+1][i].value = sigmoid(v)
-output = [NN[-1][x].value for x in range(len(NN[-1]))]
-fcost = cost(truthtable, output)
-print("cost:", fcost)
-display_NN()
-
-#print(NN[1][0][0].weights)
 print("code took: ", time.time() - start_time, "seconds to run")
